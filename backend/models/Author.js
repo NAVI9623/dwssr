5a135181f320ca8133fe67f001a1ff055bdf81e8
NAVI9623 <ivan9ramirez6@gmail.com>
2026-03-11 16:58:42 +0000

    ⭐feat: Add author route and view, update index page with author link

---
+++ backend/models/Author.js
@@ -0,0 +1,11 @@
+class Author {
+  constructor(name, description, email = '', website = '') {
+    this.name = name;
+    this.description = description;
+    this.email = email;
+    
+    this.createdAt = new Date();
+  }
+}
+
+module.exports = Author;
